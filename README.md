cb7d4cca8b3ba4e86e3001a8fad06a17fc64ecaf
Githeo <matteo.sammarco@axa.com>
2019-12-09 17:11:56 +0100

    adding doc

--- README.md
+++ README.md
@@ -1,13 +1,27 @@
 # DeepCrashzam-Dataset
-Script to create the crash sound dataset
-
+Script to create the crash sound dataset. Sounds are recorded inside the car cabin with dashcam or smartphone.
+Categories:
+- Crash
+- Driving
+- Tire skidding
+- Horn
+- Harsh acceleration
+- People taking
+- People screaming
+- Music
 
 ## Requirements
+In order to run this script, you should have already installed:
 - youtube-dl
 - sox
 
 ## Files
 - dasetCreation.sh: the main script
-- youtube_IDs.csv: 
+- youtube_IDs.csv: list of youtube videos.
+- labels: folder with txt files, each with the annotation [start time] [end time] [class] 
 
 ## Script running
+```
+$ bash datasetCreation.sh ./labels/ ./output
+```
+This will create a output folder with a sub-folder per category, including wav files.
--- datasetCreation.sh
+++ datasetCreation.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 #
-# This script fetches and trims the audio track from youtube videos.
-# It takes advantage of youtube-dl and youtube2wav.sh scripts.
-# youtube-dl is installable with brew.
-# The script takes two inputs:
-#  - a text file listing the youtube links and the relative trimming
-#  - and output folder where to store trimmed audio files (wav)
-#
-# The text file has the following structure:
-# [youtube link file] [comma separated list of trims]
-# trims have this structure of example: 3:19-0:05, where 3:19 is the starting point in
-# the video and 0:05 is de duration of the trim
-#
-# The output folder will contain only trimmed wav files.
-#
-# Comment with '#' lines to skip in the [video_list] file
-#
-# Example: bash youtubeScraping.sh youtubeFileList_path folder_to_save_clips_path 1 8000
+# Example: bash datasetCreation.sh -/labels ./output
 
 IFS=$'\n'
 
@@ -33,8 +17,13 @@ labelsFolder=$1
 outputFolder=$2
 [ ! -e ${outputFolder} ] && mkdir ${outputFolder}
 
+fileCounter=1
+totFiles=$(ls ${labelsFolder} | grep txt | wc -l)
 for labelFile in $(ls ${labelsFolder} | grep txt); do
   counter=1
+  fileCounter=$(($fileCounter+1))
+  progress=$(echo "scale=2;${fileCounter}/${totFiles}*100" | bc -l)
+  echo "Progress=${progress}%"
   videoID=$(echo ${labelFile} | cut -d '.' -f1)
   echo -e "Downloading and trimming file ${labelFile} ..."
   youtube-dl -f bestaudio --extract-audio --audio-format wav --audio-quality 0  --output "%(id)s.%(ext)s" "https://www.youtube.com/watch?v=${videoID}"
